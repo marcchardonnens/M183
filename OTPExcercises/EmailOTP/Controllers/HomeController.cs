555b41348f23376af4a7c2e2fc7a11201d217852
surfer412 <marcchardonnens@gmx.ch>
2018-09-20 23:56:23 +0200

    Email OTP progress

--- OTPExcercises/EmailOTP/Controllers/HomeController.cs
+++ OTPExcercises/EmailOTP/Controllers/HomeController.cs
@@ -3,32 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EmailOTP;
+using EmailOTP.ViewModels;
+using EmailOTP.Models;
 
 namespace EmailOTP.Controllers
 {
     public class HomeController : Controller
     {
+        Login ExampleLogin = new Login("marcchardonnens1@gmail.com","password");
         // GET: Home
+        
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(LogInViewModel vm)
+        {
+            if(vm.Email == ExampleLogin.Email && vm.Password == ExampleLogin.Password)
+            {
+                
+                //generate verification number and send email
+                ExampleLogin.SendVerificationPW();
+
+                 
+                return View("Verification", new VerificationViewModel());
+            }
+
+            return View("Login",vm);
+        }
+
         public ActionResult Index()
         {
-            ViewModels.LogInViewModel vm = new ViewModels.LogInViewModel();
-            
 
-            return View("index",vm);
+
+
+
+            return View(new LogInViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Verification(VerificationViewModel vm)
+        {
+
+            
+            return View("verification");
         }
 
 
         [HttpPost]
-        public ActionResult Verification(ViewModels.LogInViewModel viewmodel)
+        public ActionResult Verify(VerificationViewModel vm)
         {
+            if(vm.VerificationCodeInput == ExampleLogin.VerificationNumber.ToString())
+            {
+                return RedirectToAction(nameof(Index));
+            }
 
-            Models.LoginModel model = new Models.LoginModel();
-
-
-
-
-
-            return View("verification");
+            return RedirectToAction("Verification");
+            
         }
+
     }
 }
---
+++ OTPExcercises/EmailOTP/Models/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmailOTP.Models
+{
+    public class Login
+    {
+        public Login(string iEmail, string iPassword)
+        {
+            Email = iEmail;
+            Password = iPassword;
+        }
+
+        public void SendVerificationPW()
+        {
+            Random random = new Random();
+            verificationNumber = random.Next(999999);
+
+            //send email
+            
+        }
+        public string Email { get;}
+        public string Password { get;}
+        private int? verificationNumber = null;
+
+        public int? VerificationNumber { get { return verificationNumber; } }
+
+    }
+}
