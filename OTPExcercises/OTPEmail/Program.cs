b97ca094fae6f70ec8f10dd487fb953c51a1d319
surfer412 <marcchardonnens@gmx.ch>
2019-01-09 23:43:15 +0100

    SSO progress

--- OTPExcercises/OTPEmail/Program.cs
+++ OTPExcercises/OTPEmail/Program.cs
@@ -15,7 +15,8 @@ namespace OTPEmail
             var client = new SmtpClient("smtp.mailgun.org", 587)
             {
                 Credentials = new System.Net.NetworkCredential("postmaster@m183.tk", "a0dd02505f71a90ea50aff0d7f60546a-060550c6-0d2b2677"),
-                EnableSsl = true
+                EnableSsl = true,
+               
                 
                
             };
