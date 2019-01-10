39a0baa8b07d1b36270a889d427c34d533bd5a13
Marc Chardonnens <marc.chardonnens@besi.com>
2019-01-10 16:57:00 +0100

    Added View and few Actions for alternative login

--- OTPExcercises/Authentication/Controllers/MiscController.cs
+++ OTPExcercises/Authentication/Controllers/MiscController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace Authentication.Controllers
 {
     public class MiscController : Controller
     {
 
+        SqlConnection s = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=..\\Sql_XssVulnerableDB\\sql_xss_injection.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public ActionResult XssVulnerableLogin()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string Username, string Password)
+        {
 
 
+            return View();
+        }
+
+        public ActionResult GiveFeedback(string Feedback)
+        {
+
+
+            return View();
+        }
 
     }
 }
