8c637847122e76de082751b42cba698ba1449a21
surfer412 <marcchardonnens@gmx.ch>
2019-01-14 00:48:37 +0100

    database fix and migration cleanup

--- M183Solution/M183/M183/Migrations/201901132346495_init.cs
+++ M183Solution/M183/M183/Migrations/201901132346495_init.cs
@@ -3,16 +3,14 @@ namespace M183.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class addedUserSecrect2 : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.AspNetUsers", "GoogleAuthSecret", c => c.String());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.AspNetUsers", "GoogleAuthSecret");
         }
     }
 }
